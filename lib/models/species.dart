80d07f81569366937be55f320128e2955c5d31e5
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2022-05-12 22:30:08 +0200

    fix: add missing species fields

--- lib/models/species.dart
+++ lib/models/species.dart
@@ -1,1 +1,42 @@
-class Species {}
+import 'package:powerd6/models/paragraph.dart';
+
+class Species {
+  final String name;
+  final bool playable;
+  final int baseHealth;
+  final Paragraph description;
+  final Paragraph appearance;
+  final Paragraph culture;
+  final Paragraph bonuses;
+
+  Species(
+      {required this.name,
+      this.playable = false,
+      required this.baseHealth,
+      required this.description,
+      required this.appearance,
+      required this.culture,
+      required this.bonuses});
+
+  @override
+  operator ==(Object other) {
+    return other is Species &&
+        name == other.name &&
+        playable == other.playable &&
+        baseHealth == other.baseHealth &&
+        description == other.description &&
+        appearance == other.appearance &&
+        culture == other.culture &&
+        bonuses == other.bonuses;
+  }
+
+  @override
+  int get hashCode =>
+      name.hashCode ^
+      playable.hashCode ^
+      baseHealth.hashCode ^
+      description.hashCode ^
+      appearance.hashCode ^
+      culture.hashCode ^
+      bonuses.hashCode;
+}
